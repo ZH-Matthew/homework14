633b7d3715115b4538ce78c2254722a1af8aeef1
ZH-Matthew <m.zhitenev23@gmail.com>
2023-05-14 20:00:00 +0300

    Homework 14 v1 (так как мне хотелось бы изначально, без перегрузки)

---
+++ src/ServiceStation.java
@@ -0,0 +1,15 @@
+public interface ServiceStation {
+    void check();
+
+     default void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    default void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    default void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+}
---
+++ src/Vehicles.java
@@ -0,0 +1,25 @@
+public abstract class Vehicles implements ServiceStation{
+    private String modelName;
+    private int wheelsCount;
+
+    public Vehicles(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+}
